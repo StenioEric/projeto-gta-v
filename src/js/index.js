b08d5b54a2e89547e15aba6e21dfb167c36288b5
Stenio Eric <stenioeric@gmail.com>
2024-02-01 20:49:15 -0300

    Adicionado botão por JS

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+const botao = document.querySelector(".btn-plataformas")
+const elementoPlataformas = document.querySelector(".btn-plataformas .plataformas")
+
+botao.addEventListener("click", () => {
+    elementoPlataformas.classList.toggle("ativo");
+});
